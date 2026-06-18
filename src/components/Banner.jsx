bfe18bd6d86ba2d7d04cfeb17d71fcff7b93ad54
jituman01 <naimulislamjitu585@gmail.com>
2026-06-18 11:04:06 +0600

    create navbar and implement better auth and mongodb with environment variable

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 import dns from "node:dns";
@@ -38,7 +39,7 @@ export default function RootLayout({ children }) {
           <main className="max-w-7xl mx-auto px-2 min-h-screen">
             {children}
           </main>
-           
+           <Footer />
 
         </ThemeProvider>
       </body>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,213 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button, Dropdown, Label } from "@heroui/react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState,useEffect } from "react";
+import { BiLogOut } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { MdDashboard } from "react-icons/md";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  };
+  return (
+    
+      <nav className="sticky top-0 z-50 w-full border-b border-separator bg-transparent backdrop-blur-lg">
+        <header className="mx-auto flex h-16 max-w-7xl items-center justify-between px-2">
+          <div className="flex items-center gap-4">
+            <button
+              className="md:hidden"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              aria-label="Toggle menu"
+              aria-expanded={isMenuOpen}
+            >
+              <span className="sr-only">Menu</span>
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                {isMenuOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+            <Link href={'/'}>
+              <div className="">
+                
+                <p className="font-extrabold text-2xl "><span className="text-orange-500">Recipe</span>Hub</p>
+              </div>
+            </Link>
+          </div>
+          <ul className="hidden items-center gap-6 md:flex text-orange-500">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="font-medium "
+                aria-current="page"
+              >
+                Browse Recipe
+              </Link>
+            </li>
+            
+          </ul>
+          {/* RIGHT SIDE ACTIONS: THEME TOGGLE */}
+        <div className="flex items-center gap-4">
+          {mounted && (
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              className="p-2 rounded-lg bg-transparent dark:bg-transparent cursor-pointer text-slate-700 dark:text-zinc-300 active:scale-95 transition-all "
+              aria-label="Toggle Theme"
+            >
+              {theme === "dark" ? (
+                <Sun className="w-5 h-5 text-amber-400 transition-transform duration-300 rotate-0 scale-100" />
+              ) : (
+                <Moon className="w-5 h-5 text-slate-700 transition-transform duration-300 rotate-0 scale-100" />
+              )}
+            </button>
+            )}
+            </div>
+         {!user && (
+            <div className="hidden items-center gap-4 md:flex font-bold">
+              <Link href="/signin">Login</Link>
+              <Link href="/signup">
+              <Button
+                variant="danger"
+              >Sign Up</Button>
+              </Link>
+            </div>
+          )}
+
+          {user && (
+            <div className="hidden items-center gap-4 md:flex">
+              <Dropdown>
+                <Dropdown.Trigger className="rounded-full">
+                  <Avatar size="sm" aria-label="Menu">
+                    <Avatar.Image
+                      referrerPolicy="no-referrer"
+                      alt="John Doe"
+                      src={user?.image}
+                    />
+                    <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+                  </Avatar>
+                </Dropdown.Trigger>
+                <Dropdown.Popover>
+                  <div className="px-3 pt-3 pb-1">
+                    <div className="flex items-center gap-2">
+                      <Avatar size="sm">
+                        <Avatar.Image alt={user?.name} src={user?.image} />
+                        <Avatar.Fallback delayMs={600}>JD</Avatar.Fallback>
+                      </Avatar>
+                      <div className="flex flex-col gap-0">
+                        <p className="text-sm leading-5 font-medium">
+                          {user?.name}
+                        </p>
+                        <p className="text-xs leading-none text-muted">
+                          {user?.email}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <Dropdown.Menu
+                    onAction={(key) => console.log(`Selected: ${key}`)}
+                  >
+                    <Dropdown.Item id="new-file" textValue="New file">
+                      <Link
+                        className="flex items-center gap-2"
+                        href={`/dashboard/${user?.role}`}
+                      >
+                        <MdDashboard />
+                        <Label>Dashboard</Label>
+                      </Link>
+                    </Dropdown.Item>
+
+                    <Dropdown.Item id="copy-link" textValue="Copy link">
+                      <CgProfile />
+                      <Label>Profile</Label>
+                    </Dropdown.Item>
+
+                    <Dropdown.Item
+                      id="delete-file"
+                      textValue="Delete file"
+                      variant="danger"
+                      onClick={handleSignOut}
+                    >
+                      <BiLogOut />
+                      <Label>Logout</Label>
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown.Popover>
+              </Dropdown>
+            </div>
+          )}
+        </header>
+        {isMenuOpen && (
+          <div className="border-t border-separator md:hidden">
+            <ul className="flex flex-col gap-2 p-4 text-orange-500">
+              <li>
+                <Link href="/" className="block py-2">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="block py-2">
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="block py-2 font-medium text-accent">
+                  Dashboard
+                </Link>
+              </li>
+              
+              <li className="mt-4 flex flex-col gap-2 border-t border-separator pt-4">
+                <Link href="#" className="block py-2">
+                  Login
+                </Link>
+              <Button
+                variant="danger"
+                className="w-full">Sign Up</Button>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    
+  );
+};
+
+export default Navbar;
